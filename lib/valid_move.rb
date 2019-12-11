0202ea724cc2b73161943d997d1bffac7e5f2b05
Benjamin Horns <benhorns@gmail.com>
2019-12-11 19:15:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,10 @@
 require "pry"
 def valid_move?(board, index)
-  if position_taken?(board, index)
-    
 
-if index.between?(0, 8) && index != position_taken?(board, index)
+position_taken?(board, index)
+
+
+index.between?(0, 8) && index != position_taken?(board, index)
 
 
 end
