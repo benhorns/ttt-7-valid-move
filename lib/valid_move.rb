cdac90e597bbccf7691f97ddf071149025f2d311
Benjamin Horns <benhorns@gmail.com>
2019-12-11 19:21:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,14 @@
 require "pry"
 def valid_move?(board, index)
 
-index.between?(0, 8) 
+index.between?(0, 8)
+!position_taken (board,index)
 
 end
 
 def position_taken? (board, index)
 board[0] == " " || board[0]=="" || board==nil
 board[0]== "X" || board [0]=="O"
- end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
